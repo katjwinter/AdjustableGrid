feeb534f563a8801f215e1cd3b3843c33e958bdc
Kat Winter-Lewis <katjwinter@gmail.com>
2014-06-17 07:20:01 -0400

    initial commit

--- Grid.js
+++ Grid.js
@@ -1,9 +1,89 @@
-// We know the DOM is ready because this script isn't called until the background file is triggered,
+// NOTE: We know the DOM is ready because this script isn't called until the background file is triggered,
 // which will only be triggered after the DOM has been fully created. This is per Google.
 
-// Add a div at the body level with absolute position of 0,0 and width and height of 100%.
-// This will ensure that we have a full-screen overlay over the page.
+// Add markup placeholder at the body level with which will be programmatically filled
+// with the markup produced in lineTemplate.
+// In the css file, this element will have absolute position of 0,0 and width and height of 100%
+// which will allow it to overlay the entire page so when we draw lines, they appear across the page contents.
+$("body").append( $("<cover></cover>") );
+
+var baseSelected = false;
+var comparatorSelected = false;
 
 // Draw vertical and horizontal lines to make a grid across the page, overtop of content
+var lineList = [{top:"100"},{top:"200"}, {top:"300"}, {top:"400"}];
+var lineTemplate = "<% _.each(lineList, function(line) { %>" + 
+						"<div class='hline' style='top: <%=line.top%>px;'>" +
+						"</div>" +
+					"<% } ); %>";
+var temp = (_.template(lineTemplate, {lineList: lineList}));
+$("cover").html(temp);
 
 // Attach actions to the line elements for clicking and for dragging
+$(".hline").draggable( {
+    cursor:"move",
+    containment:"cover",
+    stop:handleStop,
+    start:handleStart,
+    helper:"clone",
+    appendTo:"parent"
+});
+
+$(".hline, .thick").hover( 
+    function() {
+        var pos = $(this).position();
+        var newPos = pos.top - 5;
+        $(this).css({top:newPos});
+        $(this).addClass("thick");
+    },
+    function() {
+        $(this).removeClass("thick");
+        var pos = $(this).position();
+        var newPos = pos.top + 5;
+        $(this).css({"top":newPos});
+    }
+).click(
+    function() {
+        if (baseSelected) {
+            if (comparatorSelected) {
+                if ( $(this).attr("base") ) {
+                    $("[base='base']").removeAttr("base");
+                    $("[comparator='comparator']").removeAttr("comparator");
+                    baseSelected = false;
+                    comparatorSelected = false;
+                }
+                else if ( $(this).attr("comparator") ) {
+                    $(this).removeAttr("comparator");
+                    comparatorSelected = false;
+                }
+                else {
+                    $("[comparator='comparator']").removeAttr("comparator");
+                    $(this).attr("comparator","comparator");
+                }
+            }
+            else {
+                if ( $(this).attr("base") ) {
+                    $(this).removeAttr("base");
+                    baseSelected = false;
+                }
+                else {
+                    $(this).attr("comparator","comparator");
+                    comparatorSelected = true;
+                }
+            }
+        }
+        else {
+            $(this).attr("base", "base");
+            baseSelected = true;
+        }
+    }
+)
+
+function handleStart(event, line) {
+    line.helper.removeClass("thick");
+}
+
+function handleStop(event, line) {
+    var y = line.position.top;
+    $(this).css({"top":y});
+}
--- IconClickListener.js
+++ IconClickListener.js
@@ -1,6 +1,28 @@
 // Called when the browser action (the extension's icon) is clicked
 chrome.browserAction.onClicked.addListener(function() {
+	// Load our CSS to be available in the page
+	chrome.tabs.insertCSS( null, {
+		file: "Grid.css"
+	});
+	// Load our scripts to be available in the page.
+	// Dependencies our loaded first so our primary script
+	// can run in the same context, considering a given page
+	// may not already have jquery or underscorejs and/or
+	// the correct version.
 	chrome.tabs.executeScript( null, {
-		file: "Grid.js"
+		file: "jquery-1.6.2.min.js"
+	}, function() {
+		chrome.tabs.executeScript(null, {
+			file: "underscore-min.js"
+		}, function() {
+			chrome.tabs.executeScript(null, {
+				file: "js/jquery-ui-1.10.4.custom.min.js"
+			}, function() {
+				chrome.tabs.executeScript(null, {
+					file: "Grid.js"
+				});
+			});
+		});
+		
 	});
 });
